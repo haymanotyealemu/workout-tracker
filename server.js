c8f82d8ecfa1babb39485753634f755841c5d387
Haymanot W <haymanotalemu21@gmail.com>
2020-10-03 15:36:03 -0400

    deploying

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useUnifiedTopology: true,
+useCreateIndex: true,
+useFindAndModify: false});
 require("./routes/api-routes.js")(app);
 app.get("/",(req, res) => {
     res.sendFile(path.join(__dirname + "/public/index.html"));
